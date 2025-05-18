21c80c4f8d1b7da1e450db76b98d22edff62ce45
Ramya Munusamy <ramyamunusamy@Ramya-ka-MacBook-Air.local>
2025-05-18 10:50:19 +0530

    Changed config.json to relative path

--- PycharmProjects/PythonProject/monitor.py
+++ PycharmProjects/PythonProject/monitor.py
@@ -2,33 +2,47 @@ import requests
 import json
 import time
 import logging
+import sqlite3
 from datetime import datetime
+import os
+
+pwd = os.getcwd()
+configPath = os.path.join(pwd, 'config.json')
 
 logging.basicConfig(level=logging.INFO)
 logging.info("MonitorMate started.")
 
 def load_config():
-    with open("config.json", "r") as config_file:
+    with open(configPath, "r") as config_file:
         config = json.load(config_file)
     return config['url_to_monitor']
 
-def check_website_status(url):
+def monitor_url(url):
     try:
-        response = requests.get(url, timeout=5)
-        if response.status_code == 200:
-            log_status(f"[{datetime.now()}] {url} is UP")
-        else:
-            log_status(f"[{datetime.now()}] {url} returned status code {response.status_code}")
-    except requests.exceptions.RequestException as e:
-        log_status(f"[{datetime.now()}]  {url} is DOWN. Error: {e}")
+        start_time = time.time()
+        response = requests.get(url)
+        response_time = time.time() - start_time
+        status = 'Up' if response.status_code == 200 else 'Down'
+    except requests.exceptions.RequestException:
+        status = 'Down'
+        response_time = 0.0
+
+    log_status(f"[{datetime.now()}]  {url} is {status}. Response time {response_time}")
+    log_to_database(url, status, response_time)
+
+def log_to_database(url, status, response_time):
+    conn = sqlite3.connect('monitoring.db')
+    cur = conn.cursor()
+    cur.execute('''INSERT INTO monitor_data (url, status, response_time)
+                   VALUES (?, ?, ?)''', (url, status, response_time))
+    conn.commit()
+    conn.close()
 
 def log_status(status_message):
     with open("logs/alert_log.txt", "a") as log_file:
         log_file.write(f"{datetime.now()} - {status_message}\n")
-    while True:
-        print("Checking website status...")
-        time.sleep(2)
 
 if __name__ == "__main__":
     url_to_monitor = load_config()  # Load URL from config
-    check_website_status(url_to_monitor)
+    for url in url_to_monitor:
+        monitor_url(url)
